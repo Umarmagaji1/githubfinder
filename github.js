f8b68121b7227582f61cdae0a2ab45ab60b52992
Umarmagaji1 <39120330+Umarmagaji1@users.noreply.github.com>
2018-11-02 03:08:01 -1100

    initial commit

---
+++ github.js
@@ -0,0 +1,32 @@
+class Github {
+  constructor() {
+    this.client_id = "123ef785c444f7f0d0ee";
+    this.client_secret = "8b18c7716cc3acb24b7f8fc347b13fb73f5608e0";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
